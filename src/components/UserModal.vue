2b14fe571d4f9b81d29984fd8cb03adb2c52c807
DMDXV <Padfootdiamond@Gmail.com>
2020-09-18 20:56:13 +0100

    dashboard update

---
+++ src/components/UserModal.vue
@@ -0,0 +1,54 @@
+<template>
+    <Transition name="fade">
+        <div v-if="showing" class="fixed inset-0 w-full h-screen flex items-center justify-center bg-gray-700 bg-opacity-50">
+            <div class="relative w-full max-w-2xl bg-white shadow-lg rounded-lg p-8">
+                <button aria-label="close" class="absolute top-0 right-0 text-xl text-gray-500 my-2 mx-4" @click.prevent="close">×</button>
+                <h3 class="text-center text-xl mb-4 text-gray-900 font-bold">User Settings</h3>
+                    <form class="mb-4">
+                        <div class="">
+                            <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                                Site Role
+                            </label>
+                            <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+                                <option>Member</option>
+                                <option>Admin</option>
+                            </select>
+                        </div>
+                        <div class="flex items-center justify-between mt-4">
+                            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full" type="button">
+                                Submit Changes
+                            </button>
+                        </div>
+                    </form>
+            </div>
+        </div>
+    </Transition>
+</template>
+
+<script>
+export default {
+  props: {
+    showing: {
+      required: true,
+      type: Boolean
+    },
+    user: null
+  },
+  methods: {
+    close() {
+      this.$emit('close');
+    }
+  }
+};
+</script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.4s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
--- src/views/Users.vue
+++ src/views/Users.vue
@@ -37,7 +37,13 @@
             <td class="border px-4 py-2">Diamond</td>
             <td class="border px-4 py-2">£20.00</td>
             <td class="border px-4 py-2">24/09/2020</td>
-            <td class="border px-4 py-2">01/05/2019</td>
+            <td class="border px-4 py-2">              <button @click="openModal">
+                <svg viewBox="0 0 20 20" fill="currentColor" class="pencil-alt w-6 h-6 mx-auto">
+                  <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path>
+                  <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd"></path>
+                </svg>
+              </button></td>
+            <UserModal :showing="this.open" @close="open = false" user="Mike Heatherfield"></UserModal>
           </tr>
         </tbody>
       </table>
@@ -47,8 +53,22 @@
 
 <script>
 // @ is an alias to /src
-
+import UserModal from '@/components/UserModal';
 export default {
   name: "Home",
-};
+  data() {
+    return {
+      users: null,
+      open: false
+    }
+  },
+  components: {
+    UserModal
+  },
+    methods: {
+     openModal() {
+       this.open = true
+     }
+   }
+}
 </script>
